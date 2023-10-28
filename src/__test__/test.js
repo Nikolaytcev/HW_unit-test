a7228d1c65e251ce1fe9d24daa74906aff2f825a
Nikolay1990 <Nikolaicev1990@gmail.com>
2023-10-28 11:00:29 +0300

    added sort function test

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,7 +1,22 @@
-import getIndicator from "../js/checkHealth";
+import { getIndicator, sortHeroesForhealth } from "../js/checkHealth";
 
-test("testing health indicator of hero", () => {
+
+test("testing health of hero", () => {
     const result = getIndicator({name: 'Маг', health: 90});
     expect(result).toBe("healthy");
 })
 
+test("testing sort function", () => {
+    const heroes = [
+        {name: 'мечник', health: 10},
+        {name: 'маг', health: 100},
+        {name: 'лучник', health: 80},
+    ];
+    const result = sortHeroesForhealth(heroes);
+    expect(result).toEqual([
+        {name: 'маг', health: 100},
+        {name: 'лучник', health: 80},
+        {name: 'мечник', health: 10},
+      ])
+
+})
--- src/js/checkHealth.js
+++ src/js/checkHealth.js
@@ -1,9 +1,13 @@
-export default function getIndicator(hero) {
+export function getIndicator(hero) {
     const indicators= {
         "healthy": [50, 100],
         "wounded": [15, 49],
         "critical": [0, 14]
-    }
-    const health = ["healthy", "wounded", "critical"]
+    };
+    const health = ["healthy", "wounded", "critical"];
     return health.filter(marker => hero.health >= indicators[marker][0] && hero.health <= indicators[marker][1])[0];
-};
+}
+
+export function sortHeroesForhealth(heroes) {
+    return heroes.sort((h1, h2) => h1.health < h2.health ? 1 : -1);
+}
